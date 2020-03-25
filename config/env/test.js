dd314a4a5d19705bf4745d7b6ffc3b385b6a4303
Chris Troutner <chris.troutner@gmail.com>
2020-03-25 12:21:03 -0700

    removing bitcoin-based jwt token from config

--- config/env/test.js
+++ config/env/test.js
@@ -6,8 +6,7 @@
 
 module.exports = {
   session: 'secret-boilerplate-token',
-  privateKey: '5e821b0fe2fb1529d7b0a2ab0fb5533a7e7893d0bcfa1fc7ef1f3ee5a4842a5a', // Hexadecimal
-  publicKey: '03e6c358092a459f7da9420de770eef3e16cf3c9c54a3d3d14ac2d7f0b82af4d7d', // Hexadecimal
+  token: 'secret-jwt-token',
   database: 'mongodb://localhost:27017/jwt-fullstack-test',
   env: 'test'
 }
